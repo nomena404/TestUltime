62753cd2e71edf048ecbd2f9b6973724c4eb6209
nomena404 <lovanome1@gmail.com>
2024-02-20 22:55:05 +0100

    création Json

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
+package src;
+
+import org.json.simple.JSONObject;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main{
 
     public static void main(String[]arfv){
         System.out.println("PriintMoi");
+
+        try(FileWriter file=new FileWriter("Mimi.json")){
+            JSONObject e= new JSONObject();
+            e.put("Nom","Maelia");
+            file.write(e.toJSONString());
+            file.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
